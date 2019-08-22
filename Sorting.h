160dcecaebd7f62808a771dd72f3d8abdacb2b20
nutax <40769942+nutax@users.noreply.github.com>
2019-08-22 11:58:16 -0500

    feat(bubble): Bubble sort

--- Sorting.h
+++ Sorting.h
@@ -1,5 +1,20 @@
 #include <vector>
 template<typename T>
 struct Sorting{
-
+  
+  void Bubble(vector<T>& lista){
+	int tamano = lista.size();
+	if(tamano>1){
+		for (int j = 0; j < tamano; j++) {
+			for(int k = 0; k < tamano - 1; k++){
+          if(lista[j]<lista[k]){
+            T aux = lista[k];
+            lista[k]=lista[j];
+            lista[j] = aux;
+          }
+			}
+		}
+	}
+}
+  
 }
